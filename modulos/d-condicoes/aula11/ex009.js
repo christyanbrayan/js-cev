f593b7402b1ee6e49a66aaa46aefd010792fd1b7
christyanbrayan <christyankev@gmail.com>
2020-08-12 21:38:41 -0300

    add class 11 code

---
+++ modulos/d-condicoes/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Vivendo no ${país}`)
+if (país == 'Brasil') { // condicional composta
+		console.log('Você é Brasileiro')
+} else {
+	console.log('Você é Estrangeiro')
+}
