7382072003daf906983231f095fde18b4e38b948
christyanbrayan <christyankev@gmail.com>
2024-04-19 11:59:11 -0300

    add gh-pages

--- codigos/d-condicoes/aula11/ex009.js
+++
@@ -1,8 +0,0 @@
-var país = 'EUA'
-console.log(`Vivendo no ${país}`)
-
-if (país == 'Brasil') { // condicional composta
-		console.log('Você é Brasileiro')
-} else {
-	console.log('Você é Estrangeiro')
-}
